cd9a3e9e848a29a8ef88fa3ebe90c27348b139e4
joistick11 <joistick11@gmail.com>
2017-04-27 09:33:52 +0300

    Notebook with result for both models

--- openai-gym/taxi-env/policy_iteration.py
+++ openai-gym/taxi-env/policy_iteration.py
@@ -5,27 +5,42 @@ import time
 from gym import wrappers
 import numpy as np
 
-global V, P, gamma, number_of_states, number_of_actions
+env = gym.make('Taxi-v2')
+number_of_states = env.env.nS
+number_of_actions = env.env.nA
+
+P = env.env.P  # transition matrix
+V = np.zeros(number_of_states)  # value function
+pi = np.zeros(number_of_states, dtype=np.int)  # policy
+gamma = 0.99
 
 
 # Returns value function for state S when performing action a
 def get_reward(s, a):
+    global P, V
     v_for_a = 0
     for prob, next_state, reward, is_final in P[s][a]:
         v_for_a += prob * (reward + gamma * V[next_state] * (not is_final))
     return v_for_a
 
 
-if __name__ == '__main__':
-    env = gym.make('Taxi-v2')
-    env = wrappers.Monitor(env, 'taxi-results', force=True)
-    number_of_states = env.env.env.nS
-    number_of_actions = env.env.env.nA
+def run_episode():
+    global env, pi
+    current_state = env.reset()
+    episode_reward = 0.0
+    while True:
+        # env.render()
+        time.sleep(.05)
+        current_state, reward, done, info = env.step(pi[current_state])
+        episode_reward += reward
+        if done:
+            break
+    return episode_reward
 
-    P = env.env.env.P  # transition matrix
-    V = np.zeros(number_of_states)  # value function
-    pi = np.zeros(number_of_states, dtype=np.int)  # policy
-    gamma = 0.99
+
+if __name__ == '__main__':
+    global env, V, pi, P
+    env = wrappers.Monitor(env, 'taxi-results', force=True)
 
     number_of_episodes = 1000
 
@@ -37,6 +52,7 @@ if __name__ == '__main__':
             V[s] = get_reward(s, pi[s])
 
         # Update policy
+        i = 1
         previous_pi = pi.copy()
         for s in range(number_of_states):
             profits = np.zeros(number_of_actions)
@@ -46,6 +62,10 @@ if __name__ == '__main__':
             # choose the best action
             pi[s] = np.argmax(profits)
 
+        print(i)
+        episode_reward = run_episode()
+        print(episode_reward)
+
         # Done, moving to the testing
         if np.all(previous_pi == pi):
             print("Policy has converged")
@@ -54,15 +74,8 @@ if __name__ == '__main__':
     print("Time: " + str(end - start))
 
     overall_reward = 0.0
-    for _ in range(0, 2):
-        current_state = env.reset()
-        while True:
-            env.render()
-            time.sleep(.5)
-            current_state, reward, done, info = env.step(pi[current_state])
-            overall_reward += reward
-            if done:
-                break
+    for _ in range(0, number_of_episodes):
+        overall_reward += run_episode()
 
     print("Average reward: " + str(overall_reward / number_of_episodes))
     env.close()
--- openai-gym/taxi-env/value_iteration.py
+++ openai-gym/taxi-env/value_iteration.py
@@ -5,7 +5,11 @@ import numpy as np
 import time
 from gym import wrappers
 
-global number_of_states, number_of_actions, gamma, P
+env = gym.make('Taxi-v2')
+number_of_states = env.env.nS  # overall number of states in the env
+number_of_actions = env.env.nA  # overall number of actions on each state
+P = env.env.P  # transition matrix
+gamma = 0.99
 
 
 def update_value_function(v_current):
@@ -21,6 +25,7 @@ def update_value_function(v_current):
 
 
 def create_policy(v_inp):
+    global policy
     policy = np.zeros(number_of_states)
     for s in range(number_of_states):
         profits = np.zeros(number_of_actions)
@@ -31,14 +36,25 @@ def create_policy(v_inp):
     return policy
 
 
+def run_episode(loc_policy):
+    episode_reward = 0.0
+    current_state = env.reset()
+    j = 0
+    while True:
+        # env.render()
+        current_state, reward, done, info = env.step(loc_policy[current_state])
+        # time.sleep(0.5)
+        episode_reward += reward
+        j += 1
+        if done:
+            break
+    return episode_reward
+
+
 if __name__ == '__main__':
-    env = gym.make('Taxi-v2')
     env = wrappers.Monitor(env, 'taxi-results', force=True)
 
-    number_of_states = env.env.env.nS  # overall number of states in the env
-    number_of_actions = env.env.env.nA  # overall number of actions on each state
-    P = env.env.env.P  # transition matrix
-    gamma = 0.999999
+    gamma = 0.99
     number_of_episodes = 1000
 
     # init value function
@@ -46,9 +62,15 @@ if __name__ == '__main__':
 
     start = time.time()
     # Evaluating value-function
+    i = 1
     while True:
         v_old = v.copy()
         v = update_value_function(v)
+        print("RUN EPISODE " + str(i))
+        policy = create_policy(v).astype(np.int)
+        received_reward = run_episode(policy)
+        print("REWARD=" + str(received_reward))
+        i += 1
         if np.all(v == v_old):  # value function has converged
             print("Value function has converged")
             break
@@ -60,14 +82,7 @@ if __name__ == '__main__':
     # apply policy
     overall_reward = 0.0
     for _ in range(0, number_of_episodes):
-        current_state = env.reset()
-        while True:
-            current_state, reward, done, info = env.step(policy[current_state])
-            env.render()
-            time.sleep(0.5)
-            overall_reward += reward
-            if done:
-                break
+        overall_reward += run_episode(policy)
 
     print("Average reward: " + str(overall_reward / number_of_episodes))
     env.close()
